2a3273fd705a1d5ac75a2b025d25ef9d2abb8c49
RyanSample <sampleryan1@gmail.com>
2023-06-07 15:38:46 -0500

    added an event model and some documentation

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ Quick napkin level plan:
 2. Add user auth
 3. Rework frontend
 4. Deploy initial app
+
+## Development Commands
+Migrations:
+```
+py manage.py makemigrations [name of app]
+#optional
+py manage.py sqlmigrate [name of app] [migration number] # displays the sql schema
+py manage.py migrate
+```
--- ttm/models.py
+++ ttm/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    event_name = models.CharField(max_length=150)
+    event_start = models.DateTimeField("Start date time")
+    event_end = models.DateTimeField("End date time")
+    segment_url = models.URLField()
+    distance = models.DecimalField(max_digits=5, decimal_places=2) #km, max of 999.99
+    elevation = models.DecimalField(max_digits=6, decimal_places=2) #meters, max 9999.99
